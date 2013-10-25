fe29b4cf6694813698341b32b5a0f3e3892989b3
ISAAC <ISAAC@138.100.223.203>
2013-10-25 20:16:43 +0200

    Inicial

---
+++ src/testTravis/Suma.java
@@ -0,0 +1,10 @@
+package testTravis;
+public class Suma {
+	public double getSuma(double operando1, double operando2) {
+		return operando1 + operando2;
+	}
+
+	public double incrementa(double operando) {
+		return operando + 1;
+	}
+}
